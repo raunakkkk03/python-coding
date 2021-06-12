42247cfdd6a3f5de301283706fe4824c2c38ae48
raunakkkk03 <85750313+raunakkkk03@users.noreply.github.com>
2021-06-12 19:21:46 +0530

    Update python practice.py

--- python practice.py
+++ python practice.py
@@ -1,5 +1,9 @@
+DAY 1
 print("3.5 * 4 / 2 - 2.5 is")        
 output - 3.5 * 4 / 2 - 2.5 is
 
 print(3.5 * 4 / 2 - 2.5 )
 output-4.5
+
+# print("3.5 * 4 / 2 - 2.5 is") 
+output- nothing will print as it is an comment now
