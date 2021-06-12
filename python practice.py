305e8f8f9dcf74657c266c2920cc77f8ba15855a
raunakkkk03 <85750313+raunakkkk03@users.noreply.github.com>
2021-06-12 19:43:03 +0530

    Update python practice.py

--- python practice.py
+++ python practice.py
@@ -1,4 +1,4 @@
-DAY 1
+DAY 1 SESSION 1-THE FIRST BYTE 
 print("3.5 * 4 / 2 - 2.5 is")        
 output - 3.5 * 4 / 2 - 2.5 is
 
@@ -7,3 +7,10 @@ output-4.5
 
 # print("3.5 * 4 / 2 - 2.5 is") 
 output- nothing will print as it is an comment now
+
+'''
+for two multiple comments
+we need used this bracket
+'''
+print('raja')
+output-raja
