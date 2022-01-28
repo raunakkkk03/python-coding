141428a27b5f5b616f57528141ccae12be0e24dc
raunakkkk03 <85750313+raunakkkk03@users.noreply.github.com>
2022-01-28 16:52:07 +0530

    Update python practice.py

--- python practice.py
+++ python practice.py
@@ -626,3 +626,17 @@ prime number
 prime number
 prime number
 prime number
+TO FIND THE LCM OF TWO NUMBER
+num1,num2=eval(input())
+if num1>num2:
+    small=num2
+else:
+    small=num1
+for i in range(1,small+1):
+    if((num1%i==0 and num2%i==0)):
+        gcd=i
+lcm=(num1*num2)//gcd
+print(lcm)
+OUTPUT-
+ENTER TWO NUMBER2,3
+6
